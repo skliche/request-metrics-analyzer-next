48063a5e12506fa12c0820bba13af8838e6b0a67
Stephan Kliche <me@stephankliche.de>
2015-06-07 19:45:20 +0200

    used a jtable instead of a jlist for rendering the scenarios

--- RequestMetricsAnalyzer/src/de/ibm/issw/requestmetrics/gui/RequestMetricsGui.java
+++ RequestMetricsAnalyzer/src/de/ibm/issw/requestmetrics/gui/RequestMetricsGui.java
@@ -1,64 +1,87 @@
 package de.ibm.issw.requestmetrics.gui;
 
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.swing.JFrame;
 import javax.swing.JInternalFrame;
-import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
+import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.TableCellRenderer;
 
 import de.ibm.issw.requestmetrics.RMNode;
 import de.ibm.issw.requestmetrics.RmProcessor;
 import de.ibm.issw.requestmetrics.RmRootCase;
 
+@SuppressWarnings("serial")
 public class RequestMetricsGui extends JPanel{
 	// GUI elements
-	private static JSplitPane splitPane = new JSplitPane(0);
-	private static JInternalFrame listInternalFrame = new JInternalFrame("Use Cases List", true, false, true, true);
-	private static JInternalFrame treeInternalFrame = new JInternalFrame("Selected Use Case Tree View", true, false, true, true);
+	private static final JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
+	private static final JInternalFrame listInternalFrame = new JInternalFrame("Use Cases List", true, false, true, true);
+	private static final JInternalFrame treeInternalFrame = new JInternalFrame("Selected Use Case Tree View", true, false, true, true);
+	private static final SimpleDateFormat sdf = new SimpleDateFormat("y/MM/dd HH:mm:ss:S");
 	
 	public Dimension getMinimumSize() {
-		return new Dimension(100, 600);
+		return new Dimension(100, 800);
 	}
 
 	public Dimension getPreferredSize() {
-		return new Dimension(100, 600);
+		return new Dimension(100, 800);
 	}
 	
 	public static void createAndShowGUI(final RmProcessor processor) {
-		// populate use case list
-		JList<RmRootCase> jlist = new JList<RmRootCase>(processor.getUseCases().toArray(new RmRootCase[0])); 
-		jlist.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-
+		final JTable table = new JTable(new UsecaseTableModel(processor.getUseCases()));
+		table.setFillsViewportHeight(true);
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		table.setAutoCreateRowSorter(true);
+		// the width is currently hard coded and could be gathered from data in future
+		table.getColumnModel().getColumn(0).setMinWidth(215); 
+		table.getColumnModel().getColumn(0).setMaxWidth(515); 
+		table.getColumnModel().getColumn(1).setMaxWidth(85); 
+		table.getColumnModel().getColumn(2).setMaxWidth(85); 
+		table.getColumnModel().getColumn(3).setMaxWidth(85); 
+		
 		// add selection listener to select the use cases
-		jlist.addListSelectionListener(new ListSelectionListener() {
+		table.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
 			public void valueChanged(ListSelectionEvent event) {
-				JList<RmRootCase> list = (JList<RmRootCase>) event.getSource();
-
-				RmRootCase useCase = list.getSelectedValue();
+				RmRootCase useCase = processor.getUseCases().get(table.convertRowIndexToModel(table.getSelectedRow()));
 				RMNode rmRecRoot = processor.getUseCaseRootList().get(useCase.getRmNode().getRmRecId());
 				JPanel jpanel = new UsecasePanel(rmRecRoot, processor);
 				RequestMetricsGui.treeInternalFrame.setVisible(false);
 				RequestMetricsGui.treeInternalFrame.getContentPane().removeAll();
 				RequestMetricsGui.treeInternalFrame.add(jpanel, "Center");
 				RequestMetricsGui.treeInternalFrame.setVisible(true);
-			}
+			} 
 		});
+		
+		// we write our own cell renderer for rendering the date values
+		TableCellRenderer tableCellRenderer = new DefaultTableCellRenderer() {
+		    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+		        if( value instanceof Date) {
+		            value = sdf.format(value);
+		        }
+		        return super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+		    }
+		};
+		table.getColumnModel().getColumn(0).setCellRenderer(tableCellRenderer);
 
 		JFrame mainFrame = new JFrame("RM Records Log File Analysis Results");
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		JScrollPane listScrollPane = new JScrollPane(jlist);
+		JScrollPane listScrollPane = new JScrollPane(table);
 		listInternalFrame.add(listScrollPane, "Center");
 		listInternalFrame.setVisible(true);
 		
-		splitPane.setDividerLocation(200);
+		splitPane.setDividerLocation(250);
 		splitPane.setLeftComponent(listInternalFrame);
 		
 		JPanel jpanel = new JPanel();
@@ -72,7 +95,7 @@ public class RequestMetricsGui extends JPanel{
 			}
 		});
 		mainFrame.add(splitPane, "Center");
-		mainFrame.setSize(800, 800);
+		mainFrame.setSize(900, 800);
 		mainFrame.setVisible(true);
 	}
 }
---
+++ RequestMetricsAnalyzer/src/de/ibm/issw/requestmetrics/gui/UsecaseTableModel.java
@@ -0,0 +1,59 @@
+package de.ibm.issw.requestmetrics.gui;
+
+import java.util.List;
+
+import javax.swing.table.AbstractTableModel;
+
+import de.ibm.issw.requestmetrics.RMRecord;
+import de.ibm.issw.requestmetrics.RmRootCase;
+
+@SuppressWarnings("serial")
+public class UsecaseTableModel extends AbstractTableModel {
+	private List<RmRootCase> useCases;
+	private String[] columnNames = {"Timestamp", "Elapsed Time", "Type", "Request ID", "Details"};
+		
+	public UsecaseTableModel(List<RmRootCase> useCases) {
+		this.useCases = useCases;
+	}
+
+	@Override
+	public int getRowCount() {
+		return useCases.size();
+	}
+
+	@Override
+	public int getColumnCount() {
+		return columnNames.length;
+	}
+	
+	@Override
+	public String getColumnName(int column) {
+		return columnNames[column];
+	}
+	
+	@Override
+	public Class<?> getColumnClass(int columnIndex) {
+		RMRecord record = useCases.get(0).getRmNode();
+		
+		switch (columnIndex) {
+			case 0: return record.getLogTimeStamp().getClass();
+			case 1:	return record.getElapsedTime().getClass();
+			case 2: return record.getTypeCmp().getClass();
+			case 3: return record.getCurrentCmp().getReqid().getClass();
+			default: return record.getDetailCmp().getClass();
+		}
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		RMRecord record = useCases.get(rowIndex).getRmNode();
+		
+		switch (columnIndex) {
+			case 0: return record.getLogTimeStamp();
+			case 1:	return record.getElapsedTime();
+			case 2: return record.getTypeCmp();
+			case 3: return record.getCurrentCmp().getReqid();
+			default: return record.getDetailCmp();
+		}
+	}
+}
