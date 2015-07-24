f163458cae97a3eaf5819ec39407166603216e95
Stephan Kliche <me@stephankliche.de>
2015-07-24 17:32:31 +0200

    added deadlock prevention fror recursive retrieval of nodes

--- RequestMetricsAnalyzer/src/de/ibm/issw/requestmetrics/gui/UsecasePanel.java
+++ RequestMetricsAnalyzer/src/de/ibm/issw/requestmetrics/gui/UsecasePanel.java
@@ -1,7 +1,9 @@
 package de.ibm.issw.requestmetrics.gui;
 
 import java.awt.BorderLayout;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -13,11 +15,12 @@ import de.ibm.issw.requestmetrics.RmProcessor;
 
 @SuppressWarnings("serial")
 public class UsecasePanel extends JPanel {
+	private static final Logger LOG = Logger.getLogger(UsecasePanel.class.getName());
 	public UsecasePanel(RMNode useCaseRootNode, RmProcessor processor) {
 		setLayout(new BorderLayout());
 		
 		SortableNode root = new SortableNode(useCaseRootNode);
-		getRecursive(root, useCaseRootNode, processor);
+		getRecursiveWithDeadlockPrevention(useCaseRootNode, processor, root);
 
 		JTree tree = new JTree(root);
 		for (int i = 0; i < tree.getRowCount(); i++) {
@@ -28,15 +31,27 @@ public class UsecasePanel extends JPanel {
 		scrollpane.getViewport().add(tree);
 		add("Center", scrollpane);
 	}
+
+	private void getRecursiveWithDeadlockPrevention(RMNode useCaseRootNode, RmProcessor processor, SortableNode root) {
+		final List<Long> deadlockpreventionList = new ArrayList<Long>();
+		getRecursive(root, useCaseRootNode, processor, deadlockpreventionList);
+	}
 	
-	private void getRecursive(SortableNode node, RMNode rmNode, RmProcessor processor) {
+	private void getRecursive(SortableNode node, RMNode rmNode, RmProcessor processor, List<Long> visited) {
 		final Long rmRecId = rmNode.getData().getCurrentCmp().getReqid();
 		final List<RMNode> children = processor.findByRmRecId(rmRecId);
+		
+		// perform deadlock check
+		if(visited.contains(rmRecId)) {
+			LOG.severe("Deadlock detected in rm-record with id " + rmRecId + ": " + rmNode);
+			return;
+		} 
+		visited.add(rmRecId);
 		
 		for (final RMNode childRMRecNode : children) {
 			final SortableNode childElement = new SortableNode(childRMRecNode);
 			node.add(childElement);
-			getRecursive(childElement, childRMRecNode, processor);
+			getRecursive(childElement, childRMRecNode, processor, visited);
 		}
 	}
 	class SortableNode extends DefaultMutableTreeNode {
