5afb4b6c0ef3d500b6e3e5b6bd59d9d5d6583f78
nbrass <norman.brass@gmx.de>
2015-11-11 00:00:28 +0100

    implemented issue 26: - extended table model for child node statistics to reflect new attributes (chart color & number of childrens) of the entries

--- src/main/java/de/ibm/issw/requestmetrics/gui/statistics/ChildNodeStatisticsTableModel.java
+++ src/main/java/de/ibm/issw/requestmetrics/gui/statistics/ChildNodeStatisticsTableModel.java
@@ -1,12 +1,13 @@
 package de.ibm.issw.requestmetrics.gui.statistics;
 
+import java.awt.Color;
 import java.util.List;
 
 import javax.swing.table.AbstractTableModel;
 
 @SuppressWarnings("serial")
 public class ChildNodeStatisticsTableModel extends AbstractTableModel {
-	private static final String[] columnNames = {"Component", "Executions", "Total time"};
+	private static final String[] columnNames = {"Color", "Component", "Children", "Executions", "Total time"};
 	private List<ChildNodeStatisticsEntry> entries;
 
 	public ChildNodeStatisticsTableModel(final List<ChildNodeStatisticsEntry> entries) {
@@ -26,8 +27,10 @@ public class ChildNodeStatisticsTableModel extends AbstractTableModel {
 	@Override
 	public Class<?> getColumnClass(int columnIndex) {
 		switch (columnIndex) {
-			case 1:	return Long.class;
-			case 2: return Long.class;
+			case 0:	return Color.class;
+			case 2:	return Long.class;
+			case 3:	return Long.class;
+			case 4: return Long.class;
 			default: return String.class;
 		}
 	}
@@ -35,8 +38,10 @@ public class ChildNodeStatisticsTableModel extends AbstractTableModel {
 	@Override
 	public Object getValueAt(int rowIndex, int columnIndex) {
 		switch (columnIndex) {
-			case 1:	return entries.get(rowIndex).getNumberOfExecutions();
-			case 2: return entries.get(rowIndex).getTotalTime();
+			case 0:	return entries.get(rowIndex).getChartColor();
+			case 2:	return entries.get(rowIndex).getNumberOfChildren();
+			case 3:	return entries.get(rowIndex).getNumberOfExecutions();
+			case 4: return entries.get(rowIndex).getTotalTime();
 			default: return entries.get(rowIndex).getComponent();
 		}
 	}
