2a277ce06fa5110e7c756c489c1776c3aae90353
Florian-W <florianwegert@web.de>
2015-12-05 02:39:12 +0100

    added documentation, a constructor, some constants and prevented some NullPointerExceptions. The filter class is now instantiated

--- src/main/java/de/ibm/issw/requestmetrics/engine/filter/RootCaseFilter.java
+++ src/main/java/de/ibm/issw/requestmetrics/engine/filter/RootCaseFilter.java
@@ -1,6 +1,7 @@
 package de.ibm.issw.requestmetrics.engine.filter;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.swing.JTable;
@@ -8,30 +9,50 @@ import javax.swing.RowFilter;
 import javax.swing.RowFilter.ComparisonType;
 import javax.swing.table.TableRowSorter;
 
-import de.ibm.issw.requestmetrics.gui.RequestMetricsGui;
-import de.ibm.issw.requestmetrics.gui.UsecaseTableModel;
 import de.ibm.issw.requestmetrics.gui.CheckComboBox;
+import de.ibm.issw.requestmetrics.gui.UsecaseTableModel;
 
 public class RootCaseFilter{
-	private static JTable table;
+
 	private RowFilter<UsecaseTableModel, Object> elapsedTimeRowFilter;
 	private RowFilter<UsecaseTableModel, Object> detailFilter;
 	private RowFilter<UsecaseTableModel, Object> dateTimeStartFilter;
 	private RowFilter<UsecaseTableModel, Object> dateTimeEndFilter;
 	private RowFilter<UsecaseTableModel, Object> typeFilter;
 	private List<RowFilter<UsecaseTableModel, Object>> filters = new ArrayList<RowFilter<UsecaseTableModel, Object>>();
-
+	private JTable rootCaseTable;
+	
+	private final int TIMESTAMP_COLUMN = 1;
+	private final int ELAPSED_TIME_COLUMN = 2;
+	private final int TYPE_COLUMN = 3;
+	private final int DETAIL_COLUMN = 5;
+	
+	public RootCaseFilter(JTable rootCaseTable) {
+		this.rootCaseTable = rootCaseTable;
+	}
+	
+	/**
+	 * uses a numberfilter in the elapsed time column. whenever the input 
+	 * changes, the old filter is removed to ensure that filter is only
+	 * applied for the current input
+	 * @param userInput
+	 */
 	public void filterElapsedTime(final Object userInput) {
-		table = RequestMetricsGui.getTable();
+		//
 		if (elapsedTimeRowFilter != null && filters.contains(elapsedTimeRowFilter))
 			filters.remove(elapsedTimeRowFilter);
 		elapsedTimeRowFilter = new RowFilter<UsecaseTableModel, Object>() {
 
+			//
 			@Override
 			public boolean include(javax.swing.RowFilter.Entry<? extends UsecaseTableModel, ? extends Object> entry) {
-				Long elapsedTime = (Long) table.getModel().getValueAt((int) entry.getIdentifier(), 2);
-				if (userInput == null || elapsedTime >= (Long) userInput)
-					return true;
+				if (rootCaseTable != null){
+					Long elapsedTime = (Long) rootCaseTable.getModel().getValueAt((int) entry.getIdentifier(), ELAPSED_TIME_COLUMN);
+					if (userInput == null || elapsedTime >= (Long) userInput)
+						return true;
+					else 
+						return false;
+				} 
 				else {
 					return false;
 				}
@@ -43,12 +64,18 @@ public class RootCaseFilter{
 		buildCompoundFilter();
 	}
 	
+	/**
+	 * uses a regex filter in the detail column. whenever the input changes,
+	 * the old filter is removed to ensure that filter is only applied for
+	 * the current input
+	 * @param userInput the input we want to filter for
+	 */
 	public void filterDetails(final String userInput) {
 		if (detailFilter != null && filters.contains(detailFilter))
 			filters.remove(detailFilter);
 		
 		try {
-			detailFilter = RowFilter.regexFilter(userInput, 5);
+			detailFilter = RowFilter.regexFilter(userInput, DETAIL_COLUMN);
 		} catch (Exception e) {
 			//TODO: define exception handling
 		}
@@ -58,22 +85,44 @@ public class RootCaseFilter{
 		buildCompoundFilter();
 	}
 	
-	public void filterDateTime(final Object startDate, final Object endDate) {
+	/**
+	 * 
+	 * @param startDate
+	 */
+	public void filterStartDate(final Date startDate) {
 		if (dateTimeStartFilter != null && filters.contains(dateTimeStartFilter))
 			filters.remove(dateTimeStartFilter);
-		if (dateTimeEndFilter != null && filters.contains(dateTimeEndFilter))
-			filters.remove(dateTimeEndFilter);
 		
-		dateTimeStartFilter = RowFilter.dateFilter(ComparisonType.AFTER, null, 1);
-		dateTimeEndFilter = RowFilter.dateFilter(ComparisonType.BEFORE, null, 1);
+		dateTimeStartFilter = RowFilter.dateFilter(ComparisonType.AFTER, startDate, TIMESTAMP_COLUMN);
 		
 		if (dateTimeStartFilter != null)
 			filters.add(dateTimeStartFilter);
+		buildCompoundFilter();
+	}
+	
+	/**
+	 * uses a date filter in the timestamp column. whenever the input changes,
+	 * the old filter is removed to ensure that filter is only applied for
+	 * the current input
+	 * @param endDate
+	 */
+	public void filterEndDate(final Date endDate) {
+		if (dateTimeEndFilter != null && filters.contains(dateTimeEndFilter))
+			filters.remove(dateTimeEndFilter);
+		
+		dateTimeEndFilter = RowFilter.dateFilter(ComparisonType.BEFORE, endDate, TIMESTAMP_COLUMN);
+		
 		if (dateTimeEndFilter != null)
 			filters.add(dateTimeEndFilter);
 		buildCompoundFilter();
 	}
 	
+	/**
+	 * uses a date filter in the timestamp column. whenever the input changes,
+	 * the old filter is removed to ensure that filter is only applied for
+	 * the current input
+	 * @param comboBox
+	 */
 	public void filterType(CheckComboBox comboBox) {
 		if (typeFilter != null && filters.contains(typeFilter))
 			filters.remove(typeFilter);
@@ -81,7 +130,7 @@ public class RootCaseFilter{
 		List<RowFilter<UsecaseTableModel, Object>> typeFilterList = new ArrayList<RowFilter<UsecaseTableModel, Object>>();
 		if (comboBox.getSelectedItems() != null) {
 			for (Object type : comboBox.getSelectedItems()) {
-				RowFilter<UsecaseTableModel, Object> rowFilter = RowFilter.regexFilter(type.toString(), 3);
+				RowFilter<UsecaseTableModel, Object> rowFilter = RowFilter.regexFilter(type.toString(), TYPE_COLUMN);
 				typeFilterList.add(rowFilter);
 			}
 			typeFilter = RowFilter.orFilter(typeFilterList);
@@ -93,20 +142,29 @@ public class RootCaseFilter{
 		buildCompoundFilter();
 	}
 	
+	/**
+	 * clears the all filters and thereby ensures that the table is unfiltered
+	 * (i.e. all table entries are displayed again)
+	 */
 	public void clearFilters() {
 		filters.clear();
 
-		TableRowSorter<UsecaseTableModel> sorter = new TableRowSorter<UsecaseTableModel>((UsecaseTableModel) table.getModel());
-		table.setRowSorter(sorter);
-		sorter.setRowFilter(null);
+		if (rootCaseTable != null) {
+			TableRowSorter<UsecaseTableModel> sorter = new TableRowSorter<UsecaseTableModel>((UsecaseTableModel) rootCaseTable.getModel());
+			rootCaseTable.setRowSorter(sorter);
+			sorter.setRowFilter(null);
+		}
 	}
 	
+	/**
+	 * applies all the different filters on the table by using an "andFilter"
+	 * thereby ensures that different filters can be applied at the same time
+	 */
 	private void buildCompoundFilter() {
-		table = RequestMetricsGui.getTable();
 		RowFilter<UsecaseTableModel, Object> compoundFilter = RowFilter.andFilter(filters);
 		
-		TableRowSorter<UsecaseTableModel> sorter = new TableRowSorter<UsecaseTableModel>((UsecaseTableModel) table.getModel());
-		table.setRowSorter(sorter);
+		TableRowSorter<UsecaseTableModel> sorter = new TableRowSorter<UsecaseTableModel>((UsecaseTableModel) rootCaseTable.getModel());
+		rootCaseTable.setRowSorter(sorter);
 		sorter.setRowFilter(compoundFilter);
 	}
 }
