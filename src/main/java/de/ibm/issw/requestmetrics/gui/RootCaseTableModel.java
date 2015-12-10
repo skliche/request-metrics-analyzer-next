3ffe99ca68f72832ea073a6f0a009ca05a560c49
nbrass <norman.brass@gmx.de>
2015-12-10 20:59:54 +0100

    fixed nullpointer exceptions in tablemodel

--- src/main/java/de/ibm/issw/requestmetrics/gui/RootCaseTableModel.java
+++ src/main/java/de/ibm/issw/requestmetrics/gui/RootCaseTableModel.java
@@ -47,32 +47,38 @@ public class RootCaseTableModel extends AbstractTableModel {
 	
 	@Override
 	public Class<?> getColumnClass(int columnIndex) {
-		RMNode node = useCases.get(0).getRmNode();
-		RMRecord record = node.getData();
-		switch (columnIndex) {
-			case FILE_COLUMN_INDEX: return String.class;
-			case TIMESTAMP_COLUMN_INDEX: return record.getLogTimeStamp().getClass();
-			case ELAPSEDTIME_COLUMN_INDEX:	return Long.class;
-			case TYPE_COLUMN_INDEX: return record.getTypeCmp().getClass();
-			case REQUESTID_COLUMN_INDEX: return Long.class;
-			case DETAILS_COLUMN_INDEX: return record.getDetailCmp().getClass();
-			default: return String.class;
+		if(useCases != null && !useCases.isEmpty()) {
+			RMNode node = useCases.get(0).getRmNode();
+			RMRecord record = node.getData();
+			switch (columnIndex) {
+				case FILE_COLUMN_INDEX: return String.class;
+				case TIMESTAMP_COLUMN_INDEX: return record.getLogTimeStamp().getClass();
+				case ELAPSEDTIME_COLUMN_INDEX:	return Long.class;
+				case TYPE_COLUMN_INDEX: return record.getTypeCmp().getClass();
+				case REQUESTID_COLUMN_INDEX: return Long.class;
+				case DETAILS_COLUMN_INDEX: return record.getDetailCmp().getClass();
+				default: return String.class;
+			}
 		}
+		return String.class;
 	}
 
 	@Override
 	public Object getValueAt(int rowIndex, int columnIndex) {
-		RMNode node = useCases.get(rowIndex).getRmNode();
-		RMRecord record = node.getData();
-		
-		switch (columnIndex) {
-			case FILE_COLUMN_INDEX: return record.getLogSource();
-			case TIMESTAMP_COLUMN_INDEX: return record.getLogTimeStamp();
-			case ELAPSEDTIME_COLUMN_INDEX:	return record.getElapsedTime();
-			case TYPE_COLUMN_INDEX: return record.getTypeCmp();
-			case REQUESTID_COLUMN_INDEX: return record.getCurrentCmp().getReqid();
-			case DETAILS_COLUMN_INDEX: return record.getDetailCmp();
-			default: return "Invalid column";
+		if(useCases != null && !useCases.isEmpty()) {
+			RMNode node = useCases.get(rowIndex).getRmNode();
+			RMRecord record = node.getData();
+			
+			switch (columnIndex) {
+				case FILE_COLUMN_INDEX: return record.getLogSource();
+				case TIMESTAMP_COLUMN_INDEX: return record.getLogTimeStamp();
+				case ELAPSEDTIME_COLUMN_INDEX:	return record.getElapsedTime();
+				case TYPE_COLUMN_INDEX: return record.getTypeCmp();
+				case REQUESTID_COLUMN_INDEX: return record.getCurrentCmp().getReqid();
+				case DETAILS_COLUMN_INDEX: return record.getDetailCmp();
+				default: return "Invalid column";
+			}
 		}
+		return "No RootCases";
 	}
 }
