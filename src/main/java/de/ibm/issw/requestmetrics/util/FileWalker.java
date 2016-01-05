5172e040005969895b195d24e4f698ffc1af7f43
nbrass <norman.brass@gmx.de>
2016-01-05 23:33:44 +0100

    added new helper class for reading files recursively from the given folder

---
+++ src/main/java/de/ibm/issw/requestmetrics/util/FileWalker.java
@@ -0,0 +1,35 @@
+package de.ibm.issw.requestmetrics.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileWalker extends SimpleFileVisitor<Path> {
+	private List<File> allFiles = new ArrayList<File>();
+	
+	public FileWalker(String path) throws Exception {
+		Files.walkFileTree(Paths.get(path), this);
+	}
+
+	@Override
+	public FileVisitResult visitFile(Path file, BasicFileAttributes attributes) throws IOException {
+		allFiles.add(file.toFile());
+		return FileVisitResult.CONTINUE;
+	}
+
+	@Override
+	public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attributes) throws IOException {
+		return FileVisitResult.CONTINUE;
+	}
+
+	public List<File> getAllFiles() {
+		return allFiles;
+	}
+}
